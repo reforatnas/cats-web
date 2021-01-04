89c03ee4be457c86cdc83588e05a2e871cb68199
refo <reforatnasari95@yahoo.co.id>
2021-01-04 20:01:06 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       )
     return (
       <div className='App'>
-      <h1>Cats</h1>
+      <h1>Refo's Cats</h1>
         <SearchBox 
           placeholder ='Search Cats'
           handleChange = {this.handleChange}
